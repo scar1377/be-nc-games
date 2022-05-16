664a81683053031eeb87e44ff0dbc713685f327b
Scarlett Adams <scarlettadams@Scarletts-Work-MacBook-Pro.local>
2022-05-16 20:45:41 +0100

    complete PATCH '/api/reviews/:review_id'

--- controllers/reviews.controller.js
+++ controllers/reviews.controller.js
@@ -1,8 +1,21 @@
-const { fetchReviewById } = require("../models/reviews.model");
+const {
+  fetchReviewById,
+  updateReviewById,
+} = require("../models/reviews.model");
 
 exports.getReviewById = (req, res, next) => {
   const { review_id } = req.params;
   fetchReviewById(review_id)
+    .then((review) => {
+      res.status(200).send({ review });
+    })
+    .catch(next);
+};
+
+exports.patchReviewById = (req, res, next) => {
+  const { review_id } = req.params;
+  const body = req.body;
+  updateReviewById(review_id, body)
     .then((review) => {
       res.status(200).send({ review });
     })
